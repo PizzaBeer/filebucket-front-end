1c4f1a34d38e9abc2e1c59d49419aab2f0dee234
“Benjamski” <benjadamski@gmail.com>
2016-05-13 16:28:18 -0400

    Api-user.js Ajax calls created - signUp - signIn - signOut - changePass

---
+++ assets/scripts/auth/api-user.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const app = require('../app-data');
+
+const signUp = (success, failure, data) => {
+  $.ajax({
+    method: "POST",
+    url: app.api + '/sign-up',
+    data,
+
+  })
+  .done(success)
+  .fail(failure);
+};
+
+const signIn = (success, failure, data) => {
+  $.ajax({
+    method: "POST",
+    url: app.api + '/sign-in',
+    data,
+    dataProcessing: false,
+
+  }).done(success)
+    .fail(failure);
+};
+
+const signOut = (success, failure) => {
+  $.ajax({
+    method: "DELETE",
+    url: app.api + '/sign-out/' + app.user.id,
+    headers: {
+      Authorization: 'Token token='+ app.user.token,
+    },
+  })
+  .done(success)
+  .fail(failure);
+};
+
+const changePass = (success, failure, data) => {
+  // if (!app.user) bad;
+  $.ajax({
+    method: "PATCH",
+    url: app.api + '/change-password/' + app.user.id,
+    data,
+    headers: {
+      Authorization: 'Token token='+ app.user.token,
+    },
+  })
+  .done(success)
+  .fail(failure);
+};
+
+
+module.exports = {
+  signUp,
+  signIn,
+  signOut,
+  changePass,
+  app,
+};
