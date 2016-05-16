d4f15101816f7cb0230cab60c9460371af5c1c97
natdjerf <nadjerf@gmail.com>
2016-05-16 16:56:03 -0400

    Fix merge conflict
    Function was duplicated

--- assets/scripts/auth/events-files.js
+++ assets/scripts/auth/events-files.js
@@ -3,20 +3,21 @@
 const authApiFiles = require('./api-files');
 const authUi = require('./ui-user');
 const display = require('../display');
+const fileUi = require('./ui-files');
 
 const fileEventHandlers = () => {
 
-
   $('#application-x-www-form-urlencoded').on('submit', function(e) {
     e.preventDefault();
+    Add file-ui to file AJAX functions
     let data = new FormData(this);
-    authApiFiles.uploadFile(authUi.success, authUi.failure, data);
+    authApiFiles.uploadFile(fileUi.uploadFileSuccess, fileUi.uploadFileFailure, data);
     console.log(data);
   });
 
   $('#get-files').on('click', function(e) {
     e.preventDefault();
-    authApiFiles.getAllFiles(display.displayAllFiles, authUi.failure);
+    authApiFiles.getAllFiles(display.displayAllFiles, fileUi.failure);
   });
 
   $('#all-files').on('click', '#edit-tags', function(e) {
