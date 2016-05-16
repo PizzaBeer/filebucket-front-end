3e9c791fc951f1d06175838466353f6eea072097
natdjerf <nadjerf@gmail.com>
2016-05-16 16:30:18 -0400

    Update ui file
    Add standard success/fail for testing future ajax calls.

--- assets/scripts/auth/ui-files.js
+++ assets/scripts/auth/ui-files.js
@@ -1,14 +1,27 @@
 'use strict';
 
-const uploadFileSuccess = (data) => {
+const uploadFileSuccess = () => {
   console.log('File successfully uploaded.');
 };
 
 const uploadFileFailure = (error) => {
+  console.error(error);
+  // will create a modal for upload fail
+};
+
+
+
+const success = (data) => {
+  console.log(data);
+};
+
+const failure = (error) => {
   console.error(error);
 };
 
 module.exports = {
   uploadFileSuccess,
   uploadFileFailure,
+  success,
+  failure,
 };
