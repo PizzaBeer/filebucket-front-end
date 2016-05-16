096db94a9ce34c7c2d7970ace8948c7ff5e6d919
“Benjamski” <benjadamski@gmail.com>
2016-05-16 11:51:06 -0400

    Added require and call of filevents in index.js - const fileEvents = require('./auth/events-files.js'); - fileEvents.fileEventHandlers();

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,9 +6,11 @@
 // use require without a reference to ensure a file is bundled
 require('./example');
 const userEvents = require('./auth/events-user.js');
+const fileEvents = require('./auth/events-files.js');
 
 $(() => {
   console.log('page loaded!');
   $('.modal-trigger').leanModal();
   userEvents.userEventHandlers();
+  fileEvents.fileEventHandlers();
 });
