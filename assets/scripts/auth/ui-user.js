d79493b567f0c492e8aa3dbe043ccb2dbb9fafac
“Benjamski” <benjadamski@gmail.com>
2016-05-18 18:52:56 -0400

    Fixed Sign In on Sign up -Whoo that was a doozy
    - caught sign up data on sign up event with:
    app.server.signUpData = getFormFields(this);
    
    - Used that data when calling signIn on SignUp Success
     authApi.signIn(signInSuccess, signInFailure, app.server.signUpData);

--- assets/scripts/auth/ui-user.js
+++ assets/scripts/auth/ui-user.js
@@ -3,6 +3,7 @@
 const app = require('../app-data');
 const apiNodes = require('./api-nodes.js');
 const display = require('../display');
+const authApi = require('./api-user');
 
 const success = (data) => {
   console.log(data);
@@ -40,6 +41,8 @@ const signUpSuccess = (data) => {
   $('#sign-up').each(function(){
     this.reset();
   });
+  console.log(app.server.signUpData);
+  authApi.signIn(signInSuccess, signInFailure, app.server.signUpData);
 };
 
 const signUpFailure = (error) => {
