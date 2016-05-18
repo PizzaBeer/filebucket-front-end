87ddcecf0bb822d836ceaff369811b6be75ff5ae
cuprous <zachary.simpson.1@gmail.com>
2016-05-18 17:41:41 -0400

    Truncate modifed dates
    Remove everything in modified dates past T. This causes modified dates to only
    be displayed by YYYY-MM-DD.

--- assets/scripts/display.js
+++ assets/scripts/display.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const displayAllNodes = function(nodes) {
+  nodes.nodes.forEach((e) => e.updated_at = e.updated_at.split('T')[0]);
   let allNodesTemplate = require('./templates/all-nodes.handlebars');
   $('#all-nodes').html(allNodesTemplate({
     nodes: nodes.nodes
