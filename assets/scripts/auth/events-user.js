e607286cf36d0b1b8669463617f3e98e86e90abd
“Benjamski” <benjadamski@gmail.com>
2016-05-18 10:51:15 -0400

    Sign up Success clears form - form cleared on sign up success -   $('#sign-up').each(function(){     this.reset();     - refer to refactor log for this references

--- assets/scripts/auth/events-user.js
+++ assets/scripts/auth/events-user.js
@@ -12,7 +12,7 @@ const userEventHandlers = () => {
   $('#sign-up').on('submit', function (event) {
     event.preventDefault();
     let data = getFormFields(this);
-    // authApi.signUp(authUi.signUpSuccess, authUi.failure, data);
+    authApi.signUp(authUi.signUpSuccess, authUi.failure, data);
     console.log(data);
   });
 
